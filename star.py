053272a11d1bcf60e19835da6a2b8ab86bc59a59
leilimohammadi <119856711+leilimohammadi@users.noreply.github.com>
2022-12-11 20:41:36 +0330

    Add files via upload

---
+++ star.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 11 19:57:54 2022
+
+@author: shaver pc
+"""
+
+L=3
+for i in range (1,L+1):
+    print ((L-1)*' '+(2*i-1)* '*')
---
+++ tamrine horof.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 11 20:06:49 2022
+
+@author: shaver pc
+"""
+
+z="hi how are you?"
+num_r=z.replace('h','g')
+print ("num_r=",num_r)
+num_m=len(z)
+print("num_m",num_m)
+num_r=len(z.split(" "))
+print("num_r",num_r)
+h="hi how are you?im fine."
+num_t=len(h.split(","))
+print("num_t",num_t)
